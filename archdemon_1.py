a33ee2efc61160f47391e7945b21ef004fdd777e
ush98 <indrauw98@gmail.com>
2022-04-18 13:39:50 +0530

    Housecleaning

--- archdemon_1.py
+++ archdemon_1.py
@@ -11,7 +11,6 @@ import time
 
 """ No rebalancing."""
 
-
 #50/50 split between stock and cash. 
 #1000 coin flips. 
 #Heads = Up 1% 
@@ -22,80 +21,66 @@ stock_no = 1
 stock = 50. 
 cash = 50. 
 balance = stock / (stock+cash)
-flips = 1000
-heads_move = 1. + 0.01
+flips = 1000000
+heads_move = 1. + 0.5
 tails_move = 1. / heads_move
 
 #Prepare Pandas dataframe
-demon = pd.DataFrame(columns=['Stock', 'Cash', 'Returns'])
+demon_no_rb = pd.DataFrame(columns=['Stock', 'Cash', 'Total'])
 
 for i in range(0, flips+1, 1):
-    demon.loc[i] = [stock, cash, 0]
+    demon_no_rb.loc[i] = [stock, cash, 0]
 
 # Coin flip and fill up values
 for i in range(1, flips+1, 1): 
     coin = rd.randint(0,1)
     if (coin == 0):
-        demon.loc[i, 'Stock'] = demon.loc[i-1, 'Stock'] * heads_move
+        demon_no_rb.loc[i, 'Stock'] = demon_no_rb.loc[i-1, 'Stock'] * heads_move
     elif (coin == 1):
-        demon.loc[i, 'Stock'] = demon.loc[i-1, 'Stock'] * tails_move
+        demon_no_rb.loc[i, 'Stock'] = demon_no_rb.loc[i-1, 'Stock'] * tails_move
     
-    demon.loc[i, 'Returns'] = (demon.loc[i, 'Stock'] + demon.loc[i, 'Cash']) 
+    demon_no_rb.loc[i, 'Total'] = (demon_no_rb.loc[i, 'Stock'] + demon_no_rb.loc[i, 'Cash']) 
 
 # PLot it as as a TimeSeries graph
 plt.figure(figsize=(12,8))
-plt.plot(demon['Returns'])
-plt.title('Demon')
+plt.plot(demon_no_rb['Total'])
+plt.title('No Demon')
 plt.xlabel('Flips')
-plt.ylabel('Returns')
-#plt.show()
-
-print(demon)
-
+plt.ylabel('Total')
+plt.show()
 
 """ Rebalancing."""
-""""
+
 #50/50 split between stock and cash. 
 #1000 coin flips. 
 #Heads = Up 1% 
 #Tails = Down 0.99...%
 
-#Set starting variables
-stock_no = 1
-stock = 50. 
-cash = 50. 
-balance = stock / (stock+cash) #Proportion of the stock relative to total 
-flips = 1000
-heads_move = 1. + 0.01
-tails_move = 1. / heads_move
-
 #Prepare Pandas dataframe
-demon = pd.DataFrame(columns=['Stock', 'Cash', 'Returns'])
+demon_rb = pd.DataFrame(columns=['Stock', 'Cash', 'Total'])
 
 for i in range(0, flips+1, 1):
-    demon.loc[i] = [stock, cash, 0]
+    demon_rb.loc[i] = [stock, cash, 0]
 
 # Coin flip and fill up values
 for i in range(1, flips+1, 1): 
     coin = rd.randint(0,1)
     if (coin == 0):
-        demon.loc[i, 'Stock'] = demon.loc[i-1, 'Stock'] * heads_move
+        demon_rb.loc[i, 'Stock'] = demon_rb.loc[i-1, 'Stock'] * heads_move
     elif (coin == 1):
-        demon.loc[i, 'Stock'] = demon.loc[i-1, 'Stock'] * tails_move
+        demon_rb.loc[i, 'Stock'] = demon_rb.loc[i-1, 'Stock'] * tails_move
     
-    combined = demon.loc[i, 'Stock'] + demon.loc[i, 'Cash'] 
-    demon.loc[i, 'Stock'] = combined * balance
-    demon.loc[i, 'Cash'] = combined * balance
-    demon.loc[i, 'Returns'] = (demon.loc[i, 'Stock'] + demon.loc[i, 'Cash']) - (stock+cash)
+    combined = demon_rb.loc[i, 'Stock'] + demon_rb.loc[i, 'Cash'] 
+    demon_rb.loc[i, 'Stock'] = combined * balance
+    demon_rb.loc[i, 'Cash'] = combined * balance
+    demon_rb.loc[i, 'Total'] = (demon_rb.loc[i, 'Stock'] + demon_rb.loc[i, 'Cash']) 
 
 # PLot it as as a TimeSeries graph
 plt.figure(figsize=(12,8))
-plt.plot(demon['Returns'])
+plt.plot(demon_rb['Total'])
 plt.title('Demon')
 plt.xlabel('Flips')
-plt.ylabel('Returns')
-#plt.show()
+plt.ylabel('Total')
+plt.show()
 
-print(demon)
-
-"""
+print(demon_rb)
